03147da9bfdddd9385c217989d55dcadf8dd37e5
Daniel Pak - Desktop <danielkyungpak@gmail.com>
2017-09-12 16:19:23 -0700

    Initialize

---
+++ ParseCSV/ParseCSV/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ParseCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Daniel\Source\Repos\ParseCSV\ParseCSV\ParseCSV\SocialNetwork.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                var headers = parser.ReadLine(); //Read First Line
+                List<User> users = new List<User>();
+
+                //Parsing excel sheet minus the top row and storing it into users List
+                while (!parser.EndOfData)
+                {
+                    //Process row
+                    var line = parser.ReadLine();
+                    var values = line.Split(',');
+                    User user = new User();
+                    user.Id = values[0];
+                    user.FirstName = values[1];
+                    user.LastName = values[2];
+                    var friendValues = values[3].Split(';');
+                    List<string> friends = new List<string>();
+                    foreach (var friend in friendValues)
+                    {
+                        friends.Add(friend);
+                    }
+                    user.Friends = friends;
+
+                    users.Add(user);
+                }
+
+                //Instantiating the Alison User
+                User userAlison = new User();
+
+                //Finding the Alison User in the List of Parsed Users
+                for (int i = 0; i < users.Count; i++)
+                {
+                    if (users[i].FirstName == "Alison" && users[i].LastName == "Hodges")
+                    {
+                        userAlison = users[i];
+                    }
+
+                }
+
+                //User Input
+                System.Console.WriteLine("What degree of friendship do you want to look up for Alison Hodges? (1 or 2)");
+                var degree = System.Console.ReadLine();
+
+                //Suffix Logic 
+                string degreeSuffix = "th";
+                if (Int32.Parse(degree) == 1)
+                {
+                    degreeSuffix = "st";
+                }
+                if (Int32.Parse(degree) == 2)
+                {
+                    degreeSuffix = "nd";
+                }
+
+                //Implementation of Method to find users with Nth Relationship
+                var product = userAlison.FindUsersWithNthRelationship(Int32.Parse(degree), users);
+
+
+                //Consolidating List to a string and outputting to console
+                string output = string.Join(",", product);
+                System.Console.WriteLine("This is Alison Hodges " + degree + degreeSuffix + " degree of friends with duplicates");
+                System.Console.WriteLine(output);
+
+            }
+        }
+    }
+}
---
+++ ParseCSV/ParseCSV/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseCSV
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> Friends { get; set; }
+
+
+        public List<string> FindUsersWithNthRelationship(int degree, List<User> list)
+        {
+            /*
+            * degree [type int] - if all second degree relationships are desired a 2 will
+            be passed in
+            * return string[] - an array of type string which contains all Id's of users
+            who are related to this user based on the given degree
+             */
+            List<string> friends = new List<string>();
+            if (degree == 1)
+            {
+                friends = Friends;
+                return friends;
+            }
+            else if (degree == 2)
+            {
+                for (int j = 0; j < Friends.Count; j++)
+                {
+                    var friend = Friends[j];
+                    var user = list.Find(u => u.Id == friend);
+                    for (int l = 0; l < user.Friends.Count; l++)
+                    {
+                        friends.Add(user.Friends[l]);
+                    }
+                }
+                return friends;
+
+            }
+            else
+            {
+                return friends;
+            }
+        }
+    }
+}
+
